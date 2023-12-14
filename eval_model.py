dfa5644fc3740a7673a930ae0df678d163acc66d
Author Name <lucas.kloubert5@etu.univ-lorraine.fr>
2023-12-14 15:30:47 +0100

    validation V1

---
+++ eval_model.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+from PIL import Image
+import os
+from Image_Captioning import CNN_TO_RNN
+from few_examples_results import print_examples
+import torch.optim as optim
+from get_loader import get_loader
+from torch.utils.tensorboard import SummaryWriter
+
+
+
+def eval_CNN_to_RNN(model, eval_dataloader, loss_fn):
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.eval()
+
+    with torch.no_grad():
+        valid_loss = 0
+
+        for idx, (images, captions) in tqdm(
+            enumerate(eval_dataloader), total=len(eval_dataloader), leave=False
+        ):
+        #for images, captions in eval_dataloader:
+
+            images = images.to(device)
+            captions = captions.to(device)
+
+            batch_predicted = model(images, captions[:-1])
+            
+
+            loss_this_batch = loss_fn(
+                batch_predicted.reshape(-1, batch_predicted.shape[2]), captions.reshape(-1)
+            )
+            valid_loss += loss_this_batch.item() * images.shape[0]
+        
+    valid_loss = valid_loss / len(eval_dataloader.dataset)
+    return valid_loss
